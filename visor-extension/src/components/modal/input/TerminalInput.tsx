9b314acd8e23c8b9373d16e9805425c119dbac3f
Aap Kirsel <a.kirsel@gmail.com>
2022-04-12 19:49:22 +0300

    cache terminal to avoid excessive subscriptions

--- visor-extension/src/components/modal/input/TerminalInput.tsx
+++ visor-extension/src/components/modal/input/TerminalInput.tsx
@@ -15,75 +15,16 @@ interface InputProps {
   clearSelected: (clear: Boolean) => void;
   selectedToInput: Command;
   selected: MenuItem;
+  termLines?: string[];
 }
 
 const TerminalInput = (props: InputProps) => {
-  const [subscribed, setSubscribed] = useState(null);
-  const [lines, setLines] = useState([]);
-
-  const selection = (window as any).getSelection();
+  const [termSubscribed, setTermSubscribed] = useState(null);
+  const [termLines, setTermLines] = useState([]);
 
   useEffect(() => {
-    let subId: number;
-    let number = 0;
-    let activeSubs: [any] = [0];
-
-    window.addEventListener('message', handleHerm);
-
-    const setData = () => {
-      urbitVisor.subscribe({ app: 'herm', path: '/session//view' }).then(res => {
-        console.log(res);
-        if (res.response == 'piggyback') {
-          setSubscribed(true);
-          number = res.subscriber;
-        } else {
-          number = res.subscriber;
-          setSubscribed(true);
-        }
-      });
-    };
-    urbitVisor.require(['subscribe'], setData);
-
-    return () => {
-      Messaging.sendToBackground({ action: 'active_subscriptions' }).then(res => {
-        activeSubs = res.filter((sub: any) => sub.subscription.app == 'herm');
-        console.log(activeSubs);
-        if (activeSubs.length > 1) {
-          console.log(number);
-          window.removeEventListener('message', handleHerm);
-          Messaging.sendToBackground({ action: 'remove_subscription', data: number }).then(res =>
-            console.log(res)
-          );
-        } else {
-          window.removeEventListener('message', handleHerm);
-          urbitVisor
-            .unsubscribe(activeSubs[0].airlockID)
-            .then(res => console.log('unsubscribed terminal'));
-        }
-      });
-    };
-  }, [props.selectedToInput]);
-
-  const handleHerm = useCallback(
-    (message: any) => {
-      console.log(message);
-      if (
-        message.data.app == 'urbitVisorEvent' &&
-        message.data.event.data &&
-        message.data.event.data.lin
-      ) {
-        const dojoLine = message.data.event.data.lin.join('');
-        if (!(dojoLine.includes('dojo>') || dojoLine[0] === ';' || dojoLine[0] === '>')) {
-          setLines(previousState => [...previousState, dojoLine]);
-        } else return;
-      }
-    },
-    [lines]
-  );
-
-  useEffect(() => {
-    props.airlockResponse(lines);
-  }, [lines]);
+    props.airlockResponse(props.termLines);
+  }, [props.termLines]);
 
   return <Input {...props} persistInput={true} response={false} />;
 };
