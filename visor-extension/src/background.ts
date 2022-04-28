cc75d57766ed26747eb9fdef7f4bad0dbc77a228
Aap Kirsel <a.kirsel@gmail.com>
2022-04-28 12:42:22 +0300

    fix routing call

--- visor-extension/src/background.ts
+++ visor-extension/src/background.ts
@@ -62,7 +62,11 @@ function deletedWasActive(
 function messageListener() {
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.app == 'urbit-visor-internal') handleInternalMessage(request, sender, sendResponse);
-    else if (sender.url == "chrome-extension://oadimaacghcacmfipakhadejgalcaepg/launcher.html") handleVisorCall(request, sender, sendResponse, 'extension')
+    else if (
+      sender.url == 'chrome-extension://oadimaacghcacmfipakhadejgalcaepg/launcher.html' &&
+      request.app !== 'command-launcher'
+    )
+      handleVisorCall(request, sender, sendResponse, 'extension');
     else if (request.app == 'urbitVisor') handleVisorCall(request, sender, sendResponse, 'website');
     else if (request.app == 'command-launcher')
       handleCommandLauncherCall(request, sender, sendResponse);
@@ -433,7 +437,7 @@ function checkPerms(
   sender: any,
   sendResponse: any
 ) {
-  console.log(callType, "calltype")
+  console.log(callType, 'calltype');
   const id: string = callType === 'extension' ? sender.id : sender.origin;
   const recipient = sender.tab ? sender.tab.id : sender.id;
   const extension = state.consumer_extensions.find(sumer => sumer.id === id);
